cac679ac1be890dd3090cbd77f1624af5c76eaff
clovis <clovis.dufour@elosi.com>
2015-01-30 16:07:04 +0100

    Fin de l'application demo

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '70610747a96f76d585deecb8ee2481e9690094db4db9792668e3b080ac996395f38bf73a82d0bd9563fb6379c944c86e1ba7c52e78f2c2dbcc99d417f2e5fb13'
